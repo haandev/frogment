575756a749b2e7f285e32dd4a73356cb765fb00c
Hakan Ozoglu <mhozoglu@yandex.com.tr>
2025-06-19 15:37:42 +0000

    Update package version to 1.0.4, change main entry point to ES module, and remove CommonJS export format from tsup configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/frogment.ts'],
-  format: ['esm', 'cjs'],
+  format: ['esm'],
   dts: true,
   minify: true,
   clean: true,
